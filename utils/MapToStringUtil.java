fa30a195bf24062b6b73740b1bd6e42921debc97
hujunzheng <hujunzheng@beiquan.org>
2017-07-20 23:34:31 +0800

    提交一下 map to string 工具类

--- utils/MapToStringUtil.java
+++ utils/MapToStringUtil.java
@@ -1,8 +1,14 @@
+package com.hjzgg.stateless.common.utils;
+
 import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang3.StringUtils;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by hujunzheng on 2017/7/20.
@@ -23,6 +29,30 @@ public class MapToStringUtil {
         });
 
         return StringUtils.join(result, separator);
+    }
+
+    public static Map<String, String> maptoMapString(Map<String, ?> map) {
+         return map.entrySet().stream().collect(Collectors.toMap(
+                (entry) -> {
+                    return entry.getKey();
+                },
+                (entry) -> {
+                    if(entry.getValue().getClass().isArray()) {
+                        StringBuilder sb = new StringBuilder();
+                        for(int i=0; i<Array.getLength(entry.getValue()); ++i) {
+                            Object obj = Array.get(entry.getValue(), i);
+                            sb.append(obj.toString()).append(",");
+                        }
+                        if (sb.length() > 0) {
+                            sb.deleteCharAt(sb.length() - 1);
+                        }
+
+                        return sb.toString();
+                    } else {
+                        return entry.getValue().toString();
+                    }
+                }
+        ));
     }
 
     public static void main(String[] args) {
