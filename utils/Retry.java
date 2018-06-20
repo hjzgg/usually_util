d36a6e7c975cadef8996d6dd4f19111019a275a8
hjzgg <2570230521@qq.com>
2018-06-20 17:10:24 +0800

    Create Retry.java

---
+++ utils/Retry.java
@@ -0,0 +1,37 @@
+class Retry {
+    private static final long DEFAULT_BACK_OFF_PERIOD = 1000L;
+    private static final int DEFAULT_MAX_ATTEMPTS = 2;
+
+    private RetryTemplate retryTemplate = new RetryTemplate();
+
+    public Retry() {
+        this(DEFAULT_MAX_ATTEMPTS, DEFAULT_BACK_OFF_PERIOD);
+    }
+
+    /**
+    * @param maxAttempts 最大尝试次数
+    * */
+    public Retry(int maxAttempts) {
+        this(maxAttempts, DEFAULT_BACK_OFF_PERIOD);
+    }
+
+    /**
+     * @param maxAttempts 最大尝试次数
+     * @param backOffPeriod 尝试周期，单位：ms
+     * */
+    public Retry(int maxAttempts, long backOffPeriod) {
+        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy(maxAttempts);
+        FixedBackOffPolicy backOffPolicy = new FixedBackOffPolicy();
+        backOffPolicy.setBackOffPeriod(backOffPeriod);
+        this.retryTemplate.setRetryPolicy(retryPolicy);
+        this.retryTemplate.setBackOffPolicy(backOffPolicy);
+    }
+
+    public <T, E extends Exception> T execute(RetryCallback<T, E> retryCallback) throws E {
+        return retryTemplate.execute(retryCallback);
+    }
+
+    public <T, E extends Exception> T execute(RetryCallback<T, E> retryCallback, RecoveryCallback<T> recoveryCallback) throws E {
+        return retryTemplate.execute(retryCallback, recoveryCallback);
+    }
+}
