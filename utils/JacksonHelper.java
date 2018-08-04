6f4323f46b71791c6a20e550465f1aa3bce91411
hjzgg <2570230521@qq.com>
2018-08-04 21:47:18 +0800

    Update JacksonHelper.java

--- utils/JacksonHelper.java
+++ utils/JacksonHelper.java
@@ -1,33 +1,67 @@
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.dataformat.javaprop.JavaPropsMapper;
+import com.fasterxml.jackson.dataformat.javaprop.JavaPropsSchema;
+import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+import java.util.Properties;
 
 
 public class JacksonHelper {
+    private static Logger LOGGER = LoggerFactory.getLogger(JacksonHelper.class);
+
+    /*java bean 转 properties配置*/
+    private static final JavaPropsSchema javaPropsSchema;
+
     private static final SimpleModule module = initModule();
-    private static final ObjectMapper mapper;
+    private static final ObjectMapper objectMapper;
+    private static final JavaPropsMapper javaPropsMapper;
+    private static final XmlMapper xmlMapper;
     private static final ObjectMapper prettyMapper;
 
     public JacksonHelper() {
     }
 
     private static SimpleModule initModule() {
-        return (new SimpleModule()).addSerializer(BigDecimal.class, new BigDecimalSerializer()).addSerializer(LocalTime.class, new LocalTimeSerializer()).addDeserializer(LocalTime.class, new LocalTimeDeserializer()).addSerializer(LocalDate.class, new LocalDateSerializer()).addDeserializer(LocalDate.class, new LocalDateDeserializer()).addSerializer(LocalDateTime.class, new LocalDateTimeSerializer()).addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer());
+        return (new SimpleModule()).addSerializer(BigDecimal.class, new BigDecimalSerializer())
+                .addSerializer(LocalTime.class, new LocalTimeSerializer())
+                .addDeserializer(LocalTime.class, new LocalTimeDeserializer())
+                .addSerializer(LocalDate.class, new LocalDateSerializer())
+                .addDeserializer(LocalDate.class, new LocalDateDeserializer())
+                .addSerializer(LocalDateTime.class, new LocalDateTimeSerializer())
+                .addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer());
+    }
+
+    private static JacksonXmlModule initXmlModule() {
+        JacksonXmlModule jacksonXmlModule = new JacksonXmlModule();
+        jacksonXmlModule.addSerializer(BigDecimal.class, new BigDecimalSerializer())
+                .addSerializer(LocalTime.class, new LocalTimeSerializer())
+                .addDeserializer(LocalTime.class, new LocalTimeDeserializer())
+                .addSerializer(LocalDate.class, new LocalDateSerializer())
+                .addDeserializer(LocalDate.class, new LocalDateDeserializer())
+                .addSerializer(LocalDateTime.class, new LocalDateTimeSerializer())
+                .addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer());
+        return jacksonXmlModule;
     }
 
     public static JavaType genJavaType(Type type) {
-        return getMapper().getTypeFactory().constructType(type);
+        return getObjectMapper().getTypeFactory().constructType(type);
     }
 
-    public static ObjectMapper getMapper() {
-        return mapper;
+    public static ObjectMapper getObjectMapper() {
+        return objectMapper;
     }
 
     public static ObjectMapper getPrettyMapper() {
@@ -35,8 +69,81 @@ public class JacksonHelper {
     }
 
     static {
-        mapper = (new ObjectMapper()).registerModule(module).configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false).configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true).configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        prettyMapper = mapper.copy().configure(SerializationFeature.INDENT_OUTPUT, true);
+        xmlMapper = new XmlMapper(initXmlModule());
+        xmlMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false).configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true).configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+
+        objectMapper = (new ObjectMapper()).registerModule(module).configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false).configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true).configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+
+        javaPropsMapper = new JavaPropsMapper();
+        javaPropsMapper.registerModule(module).configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false).configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true).configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+
+        javaPropsSchema = JavaPropsSchema.emptySchema().withFirstArrayOffset(0).withWriteIndexUsingMarkers(true);
+
+        prettyMapper = objectMapper.copy().configure(SerializationFeature.INDENT_OUTPUT, true);
+    }
+
+    /**
+     * json string convert properties
+     */
+    public static Properties jsonToProperties(String jsonStr) throws ServiceException {
+        try {
+            // parse JSON
+            JsonNode jsonNodeTree = objectMapper.readTree(jsonStr);
+            // resolve to properties
+            Properties properties = javaPropsMapper.writeValueAsProperties(jsonNodeTree);
+            return properties;
+        } catch (Exception e) {
+            throw new ServiceException("500", String.format("json转properties异常，json=%s", jsonStr), e);
+        }
+    }
+
+    /**
+     * object convert properties
+     */
+    public static Properties objectToProperties(Object object, String keyPrefix) throws ServiceException {
+        try {
+            if (Objects.isNull(keyPrefix)) {
+                keyPrefix = StringUtils.EMPTY;
+            }
+            Properties properties = javaPropsMapper.writeValueAsProperties(object, javaPropsSchema.withLineIndentation(keyPrefix));
+            return properties;
+        } catch (Exception e) {
+            throw new ServiceException("500", String.format("object转properties异常，object=%s", object), e);
+        }
+    }
+
+    /**
+     * json string convert to xml string
+     */
+    public static String json2xml(String jsonStr) throws ServiceException {
+        try {
+            JsonNode jsonNode = objectMapper.readTree(jsonStr);
+            String xml = xmlMapper.writeValueAsString(jsonNode);
+            return xml;
+        } catch (Exception e) {
+            throw new ServiceException("500", "json转xml异常...", e);
+        }
+    }
+
+
+    public static String json2xml(String jsonStr, String rootPrefix, String rootSuffix) throws ServiceException {
+        String xml = json2xml(jsonStr);
+        xml = xml.replace("<ObjectNode>", rootPrefix);
+        xml = xml.replace("</ObjectNode>", rootSuffix);
+        return xml;
+    }
+
+    /**
+     * xml string convert to json string
+     */
+    public static String xml2json(String xml) throws ServiceException {
+        try {
+            JsonNode node = xmlMapper.readTree(xml);
+            String json = objectMapper.writeValueAsString(node);
+            return json;
+        } catch (Exception e) {
+            throw new ServiceException("500", "xml转json异常...", e);
+        }
     }
 }
 
@@ -45,7 +152,7 @@ class LocalDateDeserializer extends JsonDeserializer<LocalDate> {
     }
 
     public LocalDate deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
-        String dateString = ((JsonNode)jp.getCodec().readTree(jp)).asText();
+        String dateString = ((JsonNode) jp.getCodec().readTree(jp)).asText();
         return LocalDate.parse(dateString, DateTimeFormatter.ISO_LOCAL_DATE);
     }
 }
@@ -55,7 +162,7 @@ class LocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
     }
 
     public LocalDateTime deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
-        String dateTimeStr = ((JsonNode)jp.getCodec().readTree(jp)).asText();
+        String dateTimeStr = ((JsonNode) jp.getCodec().readTree(jp)).asText();
         return LocalDateTime.parse(dateTimeStr, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
     }
 }
@@ -65,7 +172,7 @@ class LocalTimeDeserializer extends JsonDeserializer<LocalTime> {
     }
 
     public LocalTime deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
-        String dateString = ((JsonNode)jp.getCodec().readTree(jp)).asText();
+        String dateString = ((JsonNode) jp.getCodec().readTree(jp)).asText();
         return LocalTime.parse(dateString, DateTimeFormatter.ISO_LOCAL_TIME);
     }
 }
